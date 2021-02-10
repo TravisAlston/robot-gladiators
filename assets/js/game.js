b48221af9a8fd23890dcd2366682b600dfcc6949
Valdeth <alstontravis6@gmail.com>
2021-02-10 01:18:50 -0700

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -91,6 +91,7 @@ for(var i= 0; i< enemyNames.length; i++) {
     }
 }
 endGame();
+}
 };
 var endGame= function() {
     if(playerHealth> 0) {
@@ -108,20 +109,49 @@ var endGame= function() {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 }
-var shop= funtion() {
+var shop= function() {
     var shopOptionPrompt= window.prompt(
         "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? please enter one: 'REFILL' , 'UPGRADE', or 'LEAVE' to make a choice."
     )
     switch(shopOptionPrompt) {
-        case 'refil':
+        case "REFILL":
+        case 'refill':
+            if(playerMoney >= 7) {
             window.alert("Refilling player's health by 20 for 7 dollars.");
 
             playerHealth= playerHealth + 20;
             playerMoney= playerMoney - 7;
+            }
+            else {
+                window.alert("you don't have enough money!");
+            }
+
             break;
-        case 'upgrade' :
+
+            case "UPGRADE":
+            case 'upgrade':
+                if(playerMoney >=7) {
             window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+            playerAttack= playerAttack + 6;
+            playerMoney = playerMoney - 7;
+                }
+            else {
+                window.alert("You don't have enough money!");
+            }
+            break;
             
+            case "LEAVE":
+            case "leave":
+            window.alert("Leaving the store.");
+            break;
+
+            default:
+                window.alert("You did not pick a valid option. Try again.");
+
+                shop();
+                break;
+
     }
 };
 startGame();
